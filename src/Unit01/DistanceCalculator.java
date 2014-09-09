5c671136555b95223f84242b6ced4d2050d3bca6
Owner <Owner@10.96.178.186>
2014-09-09 11:16:45 -0400

    First Check in

---
+++ src/Unit01/DistanceCalculator.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Unit01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Owner
+ */
+public class DistanceCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        String place1Name;
+        String place2Name;
+        double lat1, lat2, long1, long2;
+        double distance;
+        
+        System.out.print("Place 1 Name:");
+        place1Name = input.nextLine();
+        System.out.print("Latitude of " + place1Name + ": ");
+        lat1 = input.nextDouble();
+        System.out.print("Longtiude of " + place1Name + ": ");
+        long1 = input.nextDouble();
+        
+  
+        
+        
+        System.out.print("Place 2 Name:");
+        place2Name = input.nextLine();
+        System.out.print("Latitude of " + place2Name + ": ");
+        lat2 = input.nextDouble();
+        System.out.print("Longtiude of " + place2Name + ": ");
+        long2 = input.nextDouble();
+        
+        distance = 6378.8 * Math.acos(Math.sin(lat1/57.29577951)) * Math.sin(lat2/57.29577951) + Math.cos(lat1/57.29577951) * Math.cos(lat2/57.29577951) * Math.cos(long2/57.29577951 - long1/57.29577951);
+        
+        System.out.println("The distance from " + place1Name + " to " + place2Name + " is: " + distance + " kilometers as the crow flies.");
+        //6378.8 * arccos[sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1)]
+    }
+    
+}
---
+++ src/Unit01/Palindrome2v2.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Unit01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Owner
+ */
+public class Palindrome2v2 {
+public static int startIndex = 0;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String sentence = getPhrase();
+        String word = "";
+        int counter = 0;
+        int numberWords;
+        if (sentence.charAt(sentence.length() - 1) != ' '){
+            sentence += " ";
+            //System.out.println("Space added");
+        }
+        numberWords = numberOfWords(sentence);
+        for (int i = 0; i < numberWords; i++){
+            word = splitIntoWords(sentence);
+            if (checkPalindrome(word) == true){
+                counter++;
+            }
+        }
+        System.out.println("Your sentence has this number of palindrome words in it: " + counter);
+    }
+    public static boolean checkPalindrome(String word){
+        String reverse = "";
+        
+        for (int i = word.length() - 1; i >= 0; i--){
+            reverse += word.charAt(i);
+        }
+       // System.out.println("Reverse: " + reverse);
+        if (reverse.equals(word)){
+            //System.out.println("yes");
+            return true;
+            
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public static int numberOfWords(String phrase){
+        int number = 0;
+        for (int i = 0; i < phrase.length(); i ++){
+            if (phrase.charAt(i) == ' '){
+                number++;
+            }
+        }
+        //System.out.println("number of words: " + number);
+    return number;    
+    }
+    public static String getPhrase(){
+        Scanner input = new Scanner (System.in);
+        String str1;
+        System.out.print("Please enter a sentence to count number of Palindromes: ");
+        str1 = input.nextLine();
+        return str1;
+    }
+    
+    public static String splitIntoWords(String phrase){ 
+        //System.out.println("start.." + startIndex);
+        int endWordIndex = phrase.indexOf(" ", startIndex ) ;
+        int wordLength = endWordIndex - startIndex;
+        //System.out.println("length .." + wordLength);
+        String word = phrase.substring(startIndex, endWordIndex);
+        //System.out.println(word);
+        startIndex = endWordIndex + 1 ;
+        
+        return word;
+
+    }
+}
